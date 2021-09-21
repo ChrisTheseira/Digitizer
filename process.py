9adccca7c509ed6d2da0144ffb70151bb606080d
Christopher Theseira <christopher.theseira@students.mq.edu.au>
2021-09-21 12:58:02 +1000

    Update #2
    Create output folder
    Copy input to output folders with shutil library

--- process.py
+++ process.py
@@ -1,1 +1,13 @@
-print("Hello")
+import glob
+import os
+import shutil
+
+inputs = glob.glob("inputs/*")
+
+os.makedirs("outputs",exist_ok=True)
+
+for filepath in inputs:
+    output_path = filepath.replace("inputs","outputs")
+    shutil.copyfile(filepath,output_path)
+    print(filepath)
+
